05fa32e023db2e4ed693cdbca05ca10741cdde2e
holddie <holddie@163.com>
2018-05-22 10:28:49 +0800

    Initial commit

---
+++ creation/simple-factory/src/main/java/com/holddie/design/App.java
@@ -0,0 +1,17 @@
+package com.holddie.design;
+
+/**
+ * Hello world!
+ *
+ */
+
+/**
+ * 新的需求来的时候需要修改工厂类，违背了开放封闭原则
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
