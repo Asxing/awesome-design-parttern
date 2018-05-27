561a0fc6d48f00d0a42a35f11b6a6d87eed82d3e
holddie <holddie@163.com>
2018-05-27 12:15:26 +0800

    :baby_chick: 接口适配器模式

---
+++ structure/structure-adapter/src/main/java/com/holddie/design/adapter/impl/Vga2HdmiAdapter.java
@@ -0,0 +1,28 @@
+package com.holddie.design.adapter.impl;
+
+import com.holddie.design.adapter.interfac.IHdmi;
+import com.holddie.design.adapter.interfac.IVga;
+
+/**
+ * VGA 接口适配 HDMI 接口
+ * @author yangze1
+ * @version 1.0.0
+ * @email holddie@163.com
+ * @date 2018/5/27 11:37
+ */
+public class Vga2HdmiAdapter implements IHdmi {
+
+    private IVga vga;
+
+    public Vga2HdmiAdapter(IVga vga) {
+        this.vga = vga;
+    }
+
+    @Override
+    public void openHdmi() {
+        vga.openVga();
+        System.out.println("Openning HDMI device.....");
+        System.out.println("适配完成");
+    }
+
+}
---
+++ structure/structure-adapter/src/main/java/com/holddie/design/adapter/impl/VgaDisplay.java
@@ -0,0 +1,17 @@
+package com.holddie.design.adapter.impl;
+
+import com.holddie.design.adapter.interfac.IVga;
+
+/**
+ * VGA 原先具体的操作
+ * @author yangze1
+ * @version 1.0.0
+ * @email holddie@163.com
+ * @date 2018/5/27 11:38
+ */
+public class VgaDisplay implements IVga {
+    @Override
+    public void openVga() {
+        System.out.println("Openning ... VGA ... monitor");
+    }
+}
