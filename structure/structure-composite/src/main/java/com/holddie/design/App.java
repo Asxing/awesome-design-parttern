a97eca891b3cc01c008f5fcd2d6b743b0f4cd495
holddie <holddie@163.com>
2018-05-29 10:05:24 +0800

    :v: 组合模式（递归思想）常见公司组织

---
+++ structure/structure-composite/src/main/java/com/holddie/design/App.java
@@ -0,0 +1,25 @@
+package com.holddie.design;
+
+import com.holddie.design.simple_module.implementor.Developer;
+import com.holddie.design.simple_module.implementor.Manager;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        Developer developer1 = new Developer("001", 011);
+        Developer developer2 = new Developer("002", 012);
+        Developer developer3 = new Developer("003", 013);
+
+        Manager manager = new Manager("000", 1000);
+        manager.add(developer1);
+        manager.add(developer2);
+        manager.add(developer3);
+
+        manager.print();
+
+    }
+}
