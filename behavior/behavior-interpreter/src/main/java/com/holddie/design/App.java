79ee61b3502070edaca47a9bd57ff1c7ecfb5052
holddie <holddie@163.com>
2018-06-05 09:46:47 +0800

    :open_mouth: 解释器模式，可以有更高阶的操作，目前只是简单解析基本运算符

---
+++ behavior/behavior-interpreter/src/main/java/com/holddie/design/App.java
@@ -0,0 +1,57 @@
+package com.holddie.design;
+
+import com.holddie.design.abs.AbstractExpression;
+import com.holddie.design.implement.*;
+
+import java.util.Stack;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        String tokenString = "40 4 3 2 - 1 + * /";
+        Stack<AbstractExpression> stack = new Stack<>();
+
+        String[] tokenList = tokenString.split(" ");
+        for (String s :
+                tokenList) {
+            if (isOperator(s)) {
+                AbstractExpression rightExpression = stack.pop();
+                AbstractExpression leftExpression = stack.pop();
+
+                AbstractExpression operator = getOperatorInstance(s, leftExpression, rightExpression);
+                int result = operator.interpret();
+                NumberExpression numberExpression = new NumberExpression(result);
+                stack.push(numberExpression);
+            } else {
+                NumberExpression numberExpression = new NumberExpression(s);
+                stack.push(numberExpression);
+            }
+        }
+
+        System.out.println("result: " + stack.pop().interpret());
+
+    }
+
+    private static AbstractExpression getOperatorInstance(String s, AbstractExpression leftExpression, AbstractExpression rightExpression) {
+        switch (s) {
+            case "+":
+                return new PlusExpression(leftExpression, rightExpression);
+            case "-":
+                return new MinusExpression(leftExpression, rightExpression);
+            case "*":
+                return new MultiplyExpression(leftExpression, rightExpression);
+            case "/":
+                return new DivExpression(leftExpression, rightExpression);
+            default:
+                return new PlusExpression(leftExpression, rightExpression);
+        }
+    }
+
+    private static boolean isOperator(String s) {
+        return "+".equals(s) || "-".equals(s) || "*".equals(s) || "/".equals(s);
+    }
+}
