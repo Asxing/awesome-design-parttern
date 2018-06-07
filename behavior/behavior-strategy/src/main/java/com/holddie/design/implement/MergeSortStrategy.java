836cd4d1576b7dd1da907dd075b6601d6c817896
holddie <holddie@163.com>
2018-06-07 10:28:09 +0800

    :four_leaf_clover: 策略模式，最常用的一种

---
+++ behavior/behavior-strategy/src/main/java/com/holddie/design/implement/MergeSortStrategy.java
@@ -0,0 +1,19 @@
+package com.holddie.design.implement;
+
+import com.holddie.design.abstracts.AbstractSortStratrgy;
+
+import java.util.List;
+
+/**
+ * 合并排序
+ * @author yangze1
+ * @version 1.0.0
+ * @email holddie@163.com
+ * @date 2018/6/7 10:14
+ */
+public class MergeSortStrategy implements AbstractSortStratrgy {
+    @Override
+    public void sort(List<Integer> list) {
+        System.out.println("Merge Sorting ....");
+    }
+}
---
+++ behavior/behavior-strategy/src/main/java/com/holddie/design/implement/QuickSortStrategy.java
@@ -0,0 +1,19 @@
+package com.holddie.design.implement;
+
+import com.holddie.design.abstracts.AbstractSortStratrgy;
+
+import java.util.List;
+
+/**
+ * 快排序
+ * @author yangze1
+ * @version 1.0.0
+ * @email holddie@163.com
+ * @date 2018/6/7 10:13
+ */
+public class QuickSortStrategy implements AbstractSortStratrgy {
+    @Override
+    public void sort(List<Integer> list) {
+        System.out.println("QuickSort sort.....");
+    }
+}
