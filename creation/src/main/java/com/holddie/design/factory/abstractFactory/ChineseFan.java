6fb0fc1599c4f115404f3b64f1efb523d4ec1886
holddie <holddie@163.com>
2020-08-28 20:11:22 +0800

    feature: The directory structure of create

---
+++ creation/src/main/java/com/holddie/design/factory/abstractFactory/ChineseFan.java
@@ -0,0 +1,19 @@
+package com.holddie.design.factory.abstractFactory;
+
+/**
+ * @author yangze1
+ * @version 1.0.0
+ * @email holddie@163.com
+ * @date 2018/5/23 9:26
+ */
+public class ChineseFan implements IFan {
+    @Override
+    public void switchOn() {
+
+    }
+
+    @Override
+    public void switchOff() {
+
+    }
+}
---
+++ creation/src/main/java/com/holddie/design/singlton/destorysingleton/SerializationSingleton.java
@@ -0,0 +1,42 @@
+package com.holddie.design.singlton.destorysingleton;
+
+import com.holddie.design.singlton.Singleton06;
+
+import java.io.*;
+
+/**
+ * 反序列化破坏单例模式
+ * @author yangze1
+ * @version 1.0.0
+ * @email holddie@163.com
+ * @date 2018/5/24 10:30
+ */
+public class SerializationSingleton {
+
+    public static void main(String[] args) {
+        Singleton06 singleton06 = Singleton06.getInstance();
+
+        ObjectOutput out = null;
+        try {
+            out = new ObjectOutputStream(new FileOutputStream(
+                    "filename.ser"));
+            out.writeObject(singleton06);
+            out.close();
+
+            ObjectInput in = new ObjectInputStream(new FileInputStream("filename.ser"));
+            Singleton06 singleton061 = (Singleton06) in.readObject();
+            in.close();
+
+            ObjectInput in1 = new ObjectInputStream(new FileInputStream("filename.ser"));
+            Singleton06 singleton062 = (Singleton06) in1.readObject();
+            in1.close();
+
+            System.out.println(singleton06.hashCode());
+            System.out.println(singleton061.hashCode());
+            System.out.println(singleton062.hashCode());
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
